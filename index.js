67cae7cb786fad832caa2754a9a3d5aa13a5e9be
Daryll Jann <daryll@hiveempire.com>
2015-05-11 14:04:09 +1000

    Add vegemite and woop-woop

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ module.exports = postcss.plugin('postcss-ass', function(opts) {
         css.eachDecl(function transformDecl(decl) {
             decl.prop = decl.prop.replace('colour', 'color');
             decl.value = decl.value.replace('true-blue', '#0581C1');
+            decl.value = decl.value.replace('vegemite', '#461B00');
             decl.value = decl.value.replace('yeah-nah', 'none');
             decl.value = decl.value.replace('fair-dinkum', 'border-box');
             decl.value = decl.value.replace('rack-off', 'hidden');
+            decl.value = decl.value.replace('woop-woop', '-9999px');
 
             if (decl.value.indexOf('!bloody-oath') >= 0) {
                 decl.value = decl.value.replace(/\s*!bloody-oath\s*/, '');
