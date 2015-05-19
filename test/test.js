7a67e76132da1c3a334a925d054f69f9b61c391e
Steve Mao <steve.mao@healthinteract.com.au>
2015-05-19 10:15:23 +1000

    Reorder tests to make it in sync with index.js

--- test/test.js
+++ test/test.js
@@ -26,9 +26,14 @@ describe('postcss-australian-stylesheets', function() {
         test('a{ border-colour: white; }', 'a{ border-color: white; }', {}, done);
     });
 
-    // !bloody-oath to !important
-    it('converts !bloody-oath to !important', function(done) {
-        test('a{ color: gray !bloody-oath; }', 'a{ color: gray !important; }', {}, done);
+    // zed-index to z-index
+    it('converts zed-index to z-index', function(done) {
+        test('a{ zed-index: 100; }', 'a{ z-index: 100; }', {}, done);
+    });
+
+    // centre to center
+    it('converts centre to center', function(done) {
+        test('a{ text-align: centre; }', 'a{ text-align: center; }', {}, done);
     });
 
     // true-blue to blue
@@ -41,6 +46,11 @@ describe('postcss-australian-stylesheets', function() {
         test('a{ color: vegemite; }', 'a{ color: #461B00; }', {}, done);
     });
 
+    // vb-green to #2D8249
+    it('converts vb-green to #2D8249', function(done) {
+        test('a{ color: vb-green; }', 'a{ color: #2D8249; }', {}, done);
+    });
+
     // yeah-nah to none
     it('converts yeah-nah to none', function(done) {
         test('a{ border: yeah-nah; }', 'a{ border: none; }', {}, done);
@@ -51,28 +61,18 @@ describe('postcss-australian-stylesheets', function() {
         test('a{ box-sizing: fair-dinkum; }', 'a{ box-sizing: border-box; }', {}, done);
     });
 
-    // woop-woop to -9999px
-    it('converts woop-woop to -9999px', function(done) {
-        test('a{ text-indent: woop-woop; }', 'a{ text-indent: -9999px; }', {}, done);
-    });
-
     // rack-off to hidden
     it('converts rack-off to hidden', function(done) {
         test('a{ visibility: rack-off; }', 'a{ visibility: hidden; }', {}, done);
     });
 
-    // zed-index to z-index
-    it('converts zed-index to z-index', function(done) {
-        test('a{ zed-index: 100; }', 'a{ z-index: 100; }', {}, done);
+    // woop-woop to -9999px
+    it('converts woop-woop to -9999px', function(done) {
+        test('a{ text-indent: woop-woop; }', 'a{ text-indent: -9999px; }', {}, done);
     });
 
-    // vb-green to #2D8249
-    it('converts vb-green to #2D8249', function(done) {
-        test('a{ color: vb-green; }', 'a{ color: #2D8249; }', {}, done);
-    });
-
-    // centre to center
-    it('converts centre to center', function(done) {
-        test('a{ text-align: centre; }', 'a{ text-align: center; }', {}, done);
+    // !bloody-oath to !important
+    it('converts !bloody-oath to !important', function(done) {
+        test('a{ color: gray !bloody-oath; }', 'a{ color: gray !important; }', {}, done);
     });
 });
